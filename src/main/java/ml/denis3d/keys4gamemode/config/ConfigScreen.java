9353bd1f3c5795ef1e6aa1aac7364b339b451508
DenisD3D <dd.denis2544@gmail.com>
2020-07-12 21:20:20 +0100

    1.16.x initial release

--- src/main/java/ml/denis3d/keys4gamemode/config/ConfigScreen.java
+++ src/main/java/ml/denis3d/keys4gamemode/config/ConfigScreen.java
@@ -1,11 +1,13 @@
 package ml.denis3d.keys4gamemode.config;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public class ConfigScreen extends Screen {
     private final Screen parentScreen;
@@ -20,20 +22,20 @@ public class ConfigScreen extends Screen {
     public void init() {
         super.init();
         this.minecraft.keyboardListener.enableRepeatEvents(true);
-        macroOne = new TextFieldWidget(minecraft.fontRenderer, 30 + this.minecraft.fontRenderer.getStringWidth("Macro Key 1"), this.height / 2 - 27, this.width - 50 - this.minecraft.fontRenderer.getStringWidth("Macro Key 1"), 15, "Macro 1");
+        macroOne = new TextFieldWidget(minecraft.fontRenderer, 30 + this.minecraft.fontRenderer.getStringWidth("Macro Key 1"), this.height / 2 - 27, this.width - 50 - this.minecraft.fontRenderer.getStringWidth("Macro Key 1"), 15, new StringTextComponent("Macro 1"));
         this.macroOne.setMaxStringLength(256);
         this.macroOne.setText(Config.CLIENT.macroOne.get());
         this.children.add(this.macroOne);
-        macroTwo = new TextFieldWidget(minecraft.fontRenderer, 30 + this.minecraft.fontRenderer.getStringWidth("Macro Key 2"), this.height / 2 - 2, this.width - 50 - this.minecraft.fontRenderer.getStringWidth("Macro Key 1"), 15, "Macro 2");
+        macroTwo = new TextFieldWidget(minecraft.fontRenderer, 30 + this.minecraft.fontRenderer.getStringWidth("Macro Key 2"), this.height / 2 - 2, this.width - 50 - this.minecraft.fontRenderer.getStringWidth("Macro Key 1"), 15, new StringTextComponent("Macro 2"));
         this.macroTwo.setMaxStringLength(256);
         this.macroTwo.setText(Config.CLIENT.macroTwo.get());
         this.children.add(this.macroTwo);
-        macroThree = new TextFieldWidget(minecraft.fontRenderer, 30 + this.minecraft.fontRenderer.getStringWidth("Macro Key 3"), this.height / 2 + 25, this.width - 50 - this.minecraft.fontRenderer.getStringWidth("Macro Key 1"), 15, "Macro 3");
+        macroThree = new TextFieldWidget(minecraft.fontRenderer, 30 + this.minecraft.fontRenderer.getStringWidth("Macro Key 3"), this.height / 2 + 25, this.width - 50 - this.minecraft.fontRenderer.getStringWidth("Macro Key 1"), 15, new StringTextComponent("Macro 3"));
         this.macroThree.setMaxStringLength(256);
         this.macroThree.setText(Config.CLIENT.macroThree.get());
         this.children.add(this.macroThree);
 
-        this.addButton(new Button(this.width / 2 - 75, this.height - 29, 150, 20, I18n.format("gui.done"), (p_213124_1_) -> {
+        this.addButton(new Button(this.width / 2 - 75, this.height - 29, 150, 20, new TranslationTextComponent("gui.done"), (p_213124_1_) -> {
             this.onClose();
         }));
     }
@@ -47,16 +49,17 @@ public class ConfigScreen extends Screen {
     }
 
     @Override
-    public void render(int p_render_1_, int p_render_2_, float p_render_3_) {
-        this.renderBackground();
-        this.drawCenteredString(minecraft.fontRenderer, this.title.getString(), this.width / 2, 20, 16777215);
-        this.drawString(minecraft.fontRenderer, "Macro Key 1", 10, this.height / 2 - 25, 16777215);
-        this.drawString(minecraft.fontRenderer, "Macro Key 2", 10, this.height / 2, 16777215);
-        this.drawString(minecraft.fontRenderer, "Macro Key 3", 10, this.height / 2 + 25, 16777215);
-        macroOne.render(p_render_1_, p_render_2_, p_render_3_);
-        macroTwo.render(p_render_1_, p_render_2_, p_render_3_);
-        macroThree.render(p_render_1_, p_render_2_, p_render_3_);
-        super.render(p_render_1_, p_render_2_, p_render_3_);
+    public void render(MatrixStack matrixStack, int cursorX, int cursorY, float ticks)
+    {
+        this.renderBackground(matrixStack);
+        this.drawCenteredString(matrixStack, minecraft.fontRenderer, this.title.getString(), this.width / 2, 20, 16777215);
+        this.drawString(matrixStack, minecraft.fontRenderer, "Macro Key 1", 10, this.height / 2 - 25, 16777215);
+        this.drawString(matrixStack, minecraft.fontRenderer, "Macro Key 2", 10, this.height / 2, 16777215);
+        this.drawString(matrixStack, minecraft.fontRenderer, "Macro Key 3", 10, this.height / 2 + 25, 16777215);
+        macroOne.render(matrixStack, cursorX, cursorY, ticks);
+        macroTwo.render(matrixStack, cursorX, cursorY, ticks);
+        macroThree.render(matrixStack, cursorX, cursorY, ticks);
+        super.render(matrixStack, cursorX, cursorY, ticks);
     }
 
     @Override
